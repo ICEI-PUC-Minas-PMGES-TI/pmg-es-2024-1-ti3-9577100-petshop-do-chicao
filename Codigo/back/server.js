1ef0de01c1284d31303a0419cf838aaa149ef9ac
luizfelipecm1 <142749233+luizfelipecm1@users.noreply.github.com>
2024-03-13 23:50:51 -0300

    [Sprint 1] Começo da tela de login

--- Codigo/back/server.js
+++ Codigo/back/server.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+app.use(cors());
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Teste123@',
+    database: 'petshop_do_chicao'
+  });
 
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000.`);
+  app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    db.query(sql, [req.body.username, req.body.password], (err, data) => {
+        if(err) return res.json("Erro");
+        if(data.length > 0){
+            return res.json("Login bem sucedido")
+        } else {
+            return res.json("Sem resposta")
+        }
+    })
+  })
+
+
+app.listen(8081, () => {
+    console.log(`Server is running on port 8081.`);
 });
---
+++ Codigo/front/src/Login.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+
+function Login() {
+
+     const [username, setUsername] = "teste"; /*=useState('')*/
+    const [password, setPassword] = "teste"; /*= useState('')*/
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', {username, password})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
+  return (
+    <div>
+        <div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='nome'>Nome de usuário</label>
+                <input type='text' placeholder='Insira seu nome de usuário' /*onChange={ u => setUsername(u.target.value)}*//>
+            </div>
+            <div>
+                <label htmlFor='senha'>Senha</label>
+                <input type='password' placeholder='Insira sua senha' /*onChange={ p => setPassword(p.target.value)}*//>
+            </div>
+            <button>Login</button>
+        </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
