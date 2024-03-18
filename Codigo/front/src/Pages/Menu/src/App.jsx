333de6acfe7695fa74e48e41420322afc0c03bf2
zagdolly <arthurzagnoli@gmail.com>
2024-03-17 23:20:05 -0300

    #2 Menu atualizado

---
+++ Codigo/front/src/Pages/Menu/src/App.jsx
@@ -0,0 +1,17 @@
+import Menu from "./components/Menu";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div class="row">
+      <div class="col-lg-3">
+        <Menu />
+      </div>
+      <div class="col-lg-9">
+        
+      </div>         
+    </div>
+  );
+}
+
+export default App;
