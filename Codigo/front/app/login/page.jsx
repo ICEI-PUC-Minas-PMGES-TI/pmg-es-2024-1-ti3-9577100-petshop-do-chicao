2d8c5fbf0f75ec409c5665c2948e63b8af29238a
Gustavo Pereira de Oliveira <gpo0251588@gmail.com>
2024-04-07 23:27:56 -0300

    [Dev] Dando uma leve arrumada em login

--- Codigo/front/app/login/page.jsx
+++ Codigo/front/app/login/page.jsx
@@ -4,50 +4,43 @@ import React from 'react'
 import axios from 'axios';
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import logo from "./img/logo.png"
 
 import styles from '../login/loginstyle.css'
 
 function Login() {
-    const router = useRouter();
-    const [username, setUsername] =useState('');
-    const [password, setPassword] = useState('');
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const res = await axios.post('http://localhost:8081/login', { username, password });
-        if (res.data === "Login bem sucedido") {
-          router.push('/menu');
-        } else {
-          alert('Login falhou');
-        }
-      } catch (err) {
-        console.error(err);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:8081/login', { username, password });
+      if (res.data === "Login bem sucedido") {
+        router.push('/menu');
+      } else {
+        alert('Login falhou');
       }
-    };
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-<div className='formulario'>
-<h1 id='title'>Login</h1>
-<form className="form card" onSubmit={handleSubmit}>
-<div className="card_header">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <path fill="none" d="M0 0h24v24H0z"></path>
-    <path fill="currentColor" d="M4 15h2v5h12V4H6v5H4V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6zm6-4V8l5 4-5 4v-3H2v-2h8z"></path>
-  </svg>
-</div>
-<div className="field" >
-  <input className="input" name="username" type="text" placeholder="Nome" onChange={ u => setUsername(u.target.value) } id="username"/>
-</div>
-<div className="field">
-  <input className="input" name="password" type="password" placeholder="Senha" id="password" onChange={ p => setPassword(p.target.value) }/>
-</div>
-<div className="field">
-  <button className="button">Entrar</button>
-</div>
-</form>
-</div>
+    <div className='formulario'>
+      <h1 id='title'>Login</h1>
+      <form className="form card" onSubmit={handleSubmit}>
+        <div className="field" >
+          <input className="input" name="username" type="text" placeholder="Nome" onChange={u => setUsername(u.target.value)} id="username" />
+        </div>
+        <div className="field">
+          <input className="input" name="password" type="password" placeholder="Senha" id="password" onChange={p => setPassword(p.target.value)} />
+        </div>
+        <div className="field">
+          <button className="button">Entrar</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
