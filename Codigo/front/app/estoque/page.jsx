b24c95477a8b66db31e41344133c120f0055017f
luizfelipecm1 <142749233+luizfelipecm1@users.noreply.github.com>
2024-05-19 15:15:05 -0300

    [Dev] Tela estoque com lógica 100%

--- Codigo/front/app/estoque/page.jsx
+++ Codigo/front/app/estoque/page.jsx
@@ -17,6 +17,7 @@ class Estoque extends React.Component{
         super(props);
 
         this.state = {
+            id: 0,
             produto_descricao: '',
             preco: '',
             qtde: '',
@@ -45,6 +46,19 @@ class Estoque extends React.Component{
         })
     }
 
+    carregarDados = (id) =>{
+        fetch("http://localhost:8081/products"+"/"+id,{ method: 'GET'})
+        .then(resposta => resposta.json())
+        .then(produtos => {
+            this.setState({ 
+                id : produtos[0].id,
+                produto_descricao: produtos[0].produto_descricao,
+                preco: produtos[0].preco,
+                qtde: produtos[0].qtde
+             })
+        })
+    }
+
     cadastraProduto = (produto) => {
         fetch("http://localhost:8081/products",
         { method: 'POST' , 
@@ -52,11 +66,24 @@ class Estoque extends React.Component{
         body: JSON.stringify(produto)
         })
         .then(response =>{
-            console.log(response);
             if(response.ok){
                 this.buscarProduto();
             }else{
                 alert('Não foi possível adicionar produto')
+            }
+        })
+    }
+    atualizarProduto = (produto) => {
+        fetch("http://localhost:8081/products"+"/"+produto.id,
+        { method: 'PUT' , 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(produto)
+        })
+        .then(response =>{
+            if(response.ok){
+                this.buscarProduto();
+            }else{
+                alert('Não foi possível atualizar o produto')
             }
         })
     }
@@ -80,12 +107,33 @@ class Estoque extends React.Component{
     }
 
     submit = () => {
-        const produto = {
-            produto_descricao: this.state.produto_descricao,
-            preco: this.state.preco,
-            qtde: this.state.qtde
+
+        if(this.state.id == 0){
+            const produto = {
+                produto_descricao: this.state.produto_descricao,
+                preco: this.state.preco,
+                qtde: this.state.qtde
+            }
+        this.cadastraProduto(produto);
+        }else{
+            const produto = {
+                id: this.state.id,
+                produto_descricao: this.state.produto_descricao,
+                preco: this.state.preco,
+                qtde: this.state.qtde
+            }
+        this.atualizarProduto(produto);
         }
-    this.cadastraProduto();
+    }
+
+    reset = () => {
+        this.setState({
+            id: 0,
+            produto_descricao:'',
+            preco:'',
+            qtde: ''
+        })
+
     }
 
 
@@ -94,13 +142,16 @@ class Estoque extends React.Component{
             <div>
                 <div>
                 <form>
+                    <label for="iname">ID</label>
+                    <input type="text" id="iname" name="id" value={ this.state.id } readOnly={true}/>
                     <label for="fname">Descrição:</label>
                     <input type="text" id="fname" name="descricao" value={ this.state.produto_descricao } onChange={this.atualizaDescricao}/>
                     <label for="lname">Preço:</label>
                     <input type="text" id="lname" name="preco" value={ this.state.preco } onChange={this.atualizaPreco}/>
                     <label for="qname">Quantidade:</label>
-                    <input type="text" id="qname" name="quantidade" value={ this.state.qtde } onChange={this.atualizaQtde}/>
+                    <input type="text" id="qname" name="quantidade" value={ this.state.qtde } onChange={this.atualizaQtde} readOnly={true}/>
                     <Button onClick={this.submit}>Adicionar</Button>
+                    <Button onClick={this.reset}>Novo</Button>
                 </form>
                 </div>
                 <div>Tabela de Produtos</div>
@@ -116,11 +167,12 @@ class Estoque extends React.Component{
       {
                         this.state.products.map((produto) =>
                         <TableRow>
-                        <TableCell> {produto.idproducts} </TableCell>
+                        <TableCell> {produto.id} </TableCell>
                         <TableCell> {produto.produto_descricao} </TableCell>
                         <TableCell> {produto.preco} </TableCell>
                         <TableCell> {produto.qtde} </TableCell>
-                        <TableCell>  <Button>Atualizar</Button> <Button onClick={() => this.deletarProduto(produto.idproducts)}>Excluir</Button> </TableCell>
+                        <TableCell>  <Button onClick={() => this.carregarDados(produto.id)}>Atualizar</Button>
+                         <Button onClick={() => this.deletarProduto(produto.id)}>Excluir</Button> </TableCell>
                         </TableRow>
                         )
                     }
